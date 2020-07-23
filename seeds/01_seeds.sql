970bfca3deea218844d650c03f0087873e494e31
HenryMao <henry60308@live.cn>
2020-07-23 01:35:24 +0000

    insert

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (id, name, email, password) 
+VALUES (1, 'n1', 'email1', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+(2, 'n2', 'email2', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+(3, 'n3', 'email3', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
